6dfbf7ab76eeb9bc93bb1b36d39edb2d12e45c89
lipemizani <filipe.mizani99@gmail.com>
2024-03-23 14:25:15 -0300

    Alteração

--- HelloWorld/src/myfirstproject/HelloWorld.java
+++ HelloWorld/src/myfirstproject/HelloWorld.java
@@ -3,7 +3,7 @@ package myfirstproject;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Olá, Mundo!");
+		System.out.println("E DE NOVO!! Olá, Mundo!");
 
 	}
 
